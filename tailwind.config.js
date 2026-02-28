47bdb0366e54684fd4ae8d1eee6acd6f62d935d1
HopeyClarkey <thehopeclarke@gmail.com>
2026-02-28 12:47:48 -0600

    style container locations, set up tailwind/daisy config, added placeholder colors

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {},
+  plugins: [require("daisyui")],
+}
