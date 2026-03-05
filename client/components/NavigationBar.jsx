2d0703360d22082c6de8687ebe00ec31f12d7549
HopeyClarkey <thehopeclarke@gmail.com>
2026-03-05 17:54:44 -0600

    add style fixes for gallery view

--- client/components/NavigationBar.jsx
+++ client/components/NavigationBar.jsx
@@ -1,39 +1,49 @@
-export default function NavBar({ view, setView, isShowingGallery, onToggleGallery }) {
-  
+
+export default function NavBar({
+  view,
+  setView,
+  isShowingGallery,
+
+  onToggleGallery,
+}) {
   return (
     <div id="sidebar" className="sidebar">
       <div id="navigation-bar" className="navigation-bar">
         <nav>
-              {/* HomePage button */}
-              <button
-                onClick={() => {
-                  setView('HomePage');
-                }}
-              >
-                {'Go to Home'}
-              </button>
-              {/* ImageUpload Button */}
-              <button
-                onClick={() => {
-                  setView('ImageUpload');
-                }}
-              >
-                {'Go to Upload'}
-              </button>
-              {/* ImagePage button */}
-              <button
-                onClick={() => {
-                  setView('ImagePage');
-                }}
-              >
-                {'Go to Image'}
-              </button>
-              {/* Gallery show/hide button */}
-              <button onClick={onToggleGallery}>
-                {isShowingGallery ? 'Hide Gallery' : 'Show Gallery'}
-              </button>
-            {/* Page view */}
-            {view}          
+          <button
+            onClick={() => {
+              setView('HomePage');
+            }}
+          >
+            {'Go to Home'}
+          </button>
+          <br />
+
+          <button
+            onClick={() => {
+              onToggleGallery();
+              setView('ImageUpload');
+            }}
+          >
+            {'Go to Upload'}
+          </button>
+          <br />
+
+          <button
+            onClick={() => {
+              setView('ImagePage');
+            }}
+          >
+            {'Go to Image'}
+          </button>
+          <br />
+
+          <button onClick={onToggleGallery}>
+            {isShowingGallery ? 'Hide Gallery' : 'Show Gallery'}
+          </button>
+          <br />
+
+          {view}
         </nav>
       </div>
     </div>
